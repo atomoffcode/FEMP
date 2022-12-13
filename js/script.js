36d76424faba224d54001cb12c7fbfa7270d2564
atomoffcode <kamilka@code.edu.az>
2022-12-14 01:24:31 +0400

    v2

--- js/script.js
+++ js/script.js
@@ -109,30 +109,46 @@ $('.slider3').slick({
     
 });
 
+$(window).scroll(function() {
+    if ($("html,body").scrollTop() > 20) {
+        $('#myBtn').fadeIn(400);
+        $('#myBtn').css("bottom","70px")
+    } else {
+        $('#myBtn').fadeOut(400);
+        $('#myBtn').css("bottom","10px")
+
+    }
+});
+
+$("#myBtn").click(function() {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 300);
+});
 
 
-let mybutton = document.getElementById("myBtn");
+// $('html,body').onscroll()
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.bottom = "70px"
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.bottom = "70px"
     
-    mybutton.style.display = "block";
-    mybutton.style.opacity = "1";
+//     mybutton.style.display = "block";
+//     mybutton.style.opacity = "1";
 
-  } else {
-    mybutton.style.bottom = "10px"
+//   } else {
+//     mybutton.style.bottom = "10px"
 
-    mybutton.style.opacity = "0";
-    mybutton.style.display = "none";
-  }
-}
+//     mybutton.style.opacity = "0";
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0; // For Safari
+//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }
